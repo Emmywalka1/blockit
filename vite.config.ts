5988e34d23506f924cc7b74770406a663cf61528
Emmywalka1 <91231911+Emmywalka1@users.noreply.github.com>
2025-07-30 00:41:56 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,13 @@ export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'dist',
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+        },
+      },
+    },
   },
 })
