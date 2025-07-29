b14e53530112fdadfeed81c6e3068c35c71d69bd
Emmywalka1 <91231911+Emmywalka1@users.noreply.github.com>
2025-07-29 23:56:22 +0100

    Create postcss.config.js

---
+++ postcss.config.js
@@ -0,0 +1,11 @@
+// ===================================
+// FILE: postcss.config.js
+// PostCSS configuration for Tailwind
+// ===================================
+
+export default {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+}
